c3176360167bc23c4feecb52236147dc81a7ff16
Andrey Grishin <grishinandreyy@icloud.com>
2024-03-14 22:54:47 +0300

    chore: add train module
    Added train.py module. 1st file commit.

---
+++ mlops/train.py
@@ -0,0 +1,13 @@
+def train(path: str) -> None:
+    """Train the model.
+
+    Train and save the tuned weights of the model.
+
+    Args:
+        path: path to train dataset `train.tsv`
+
+    Return:
+        No return.
+    """
+
+    pass
